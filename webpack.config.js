ccca4ec243921884d5efafa69990ef63d8b4b663
Khoi Le <minarek@outlook.com>
2018-04-02 09:08:37 +0200

    Created Spotify search project

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path                  = require('path');
+const CleanWebpackPlugin    = require('clean-webpack-plugin');
+
+module.exports = {
+    devtool: 'source-map',
+
+    entry: './src/index.js',
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public/build'),
+
+        publicPath: 'build'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['.js']
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(['public/build'])
+    ],
+
+    devServer: {
+        port: 3000,
+        contentBase: './public',
+        compress: true,
+        historyApiFallback: true
+    }
+};
